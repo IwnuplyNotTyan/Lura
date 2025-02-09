712a320952486ea9791fcafda83f468592be2ebc
iwnuplynottyan <wolflast21@gmail.com>
2025-02-09 14:40:19 +0200

    weapon system?

--- main.go
+++ main.go
@@ -33,17 +33,29 @@ func main() {
 	}
 	defer db.Close()
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS app (id INTEGER PRIMARY KEY, monsterType TEXT, hp INTEGER, damage INTEGER, weaponType TEXT)")
+	// Creating separate tables for monsters and weapons
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS monsters (id INTEGER PRIMARY KEY, monsterType TEXT, hp INTEGER, damage INTEGER)")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Example usage:
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS weapons (id INTEGER PRIMARY KEY, weaponType TEXT, damage INTEGER)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	addMonster("dragon", 150, 30)
 	addMonster("human", 50, 5)
 	addMonster("cat", 5, 0)
 
-	player := Player{WeaponType: "sword", Damage: 10 * rng(), HP: 100}
+	addWeapon("sword", 10)
+	addWeapon("spear", 7)
+	addWeapon("axe", 12)
+
+	// Assigning a random weapon to the player
+	weaponType, weaponDamage := getRandomWeapon()
+	player := Player{WeaponType: weaponType, Damage: weaponDamage * rng(), HP: 100}
+
 	attack(player)
 }
 
@@ -52,8 +64,21 @@ func rng() int {
 	return rand.Intn(6) + 1 // Random number between 1 and 6
 }
 
+func addWeapon(weaponType string, damage int) {
+	stmt, err := db.Prepare("INSERT INTO weapons (weaponType, damage) VALUES (?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(weaponType, damage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func addMonster(monsterType string, hp int, damage int) {
-	stmt, err := db.Prepare("INSERT INTO app (monsterType, hp, damage) VALUES (?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO monsters (monsterType, hp, damage) VALUES (?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -65,8 +90,24 @@ func addMonster(monsterType string, hp int, damage int) {
 	}
 }
 
+func getRandomWeapon() (string, int) {
+	rows, err := db.Query("SELECT weaponType, damage FROM weapons ORDER BY RANDOM() LIMIT 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var weaponType string
+	var damage int
+	if rows.Next() {
+		rows.Scan(&weaponType, &damage)
+		return weaponType, damage
+	}
+	return "fists", 2 // Default weapon if no weapons exist
+}
+
 func getRandomMonster() *Monster {
-	rows, err := db.Query("SELECT id, monsterType, hp, damage FROM app ORDER BY RANDOM() LIMIT 1")
+	rows, err := db.Query("SELECT id, monsterType, hp, damage FROM monsters ORDER BY RANDOM() LIMIT 1")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -88,15 +129,29 @@ func attack(player Player) {
 	}
 
 	fmt.Printf("A wild %s appears with %d HP!\n", monster.MonsterType, monster.HP)
+	fmt.Printf("You wield a %s dealing %d damage and have %d HP.\n", player.WeaponType, player.Damage, player.HP)
 
-	for monster.HP > 0 {
-		damage := player.Damage + rng()
-		monster.HP -= damage
-		fmt.Printf("You hit the %s for %d damage! It now has %d HP left.\n", monster.MonsterType, damage, monster.HP)
+	for monster.HP > 0 && player.HP > 0 {
+		// Player attacks
+		playerDamage := player.Damage + rng()
+		monster.HP -= playerDamage
+		fmt.Printf("You hit the %s for %d damage! It now has %d HP left.\n", monster.MonsterType, playerDamage, monster.HP)
+
 		if monster.HP <= 0 {
 			fmt.Printf("You defeated the %s!\n", monster.MonsterType)
 			return
 		}
+
+		// Monster attacks
+		monsterDamage := monster.Damage + rng()
+		player.HP -= monsterDamage
+		fmt.Printf("The %s hits you for %d damage! You now have %d HP left.\n", monster.MonsterType, monsterDamage, player.HP)
+
+		if player.HP <= 0 {
+			fmt.Println("You died!")
+			return
+		}
+
 		time.Sleep(time.Second)
 	}
 }
