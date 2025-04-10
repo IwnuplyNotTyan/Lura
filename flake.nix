7e49edea9b4e2332ab6c955614bdcba8da4f9494
IwnuplyNotTyan <wolflast21@gmail.com>
2025-04-11 00:10:02 +0300

    snowflake, nix btw

--- flake.nix
+++ flake.nix
@@ -1,42 +1,59 @@
-
 {
-  description = "Lura";
-
+  description = "Lura ~";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.default = pkgs.buildGoModule {
-          pname = "lura";
-          version = "1.2.0";
-
-          src = ./.;
-
-          vendorHash = null;
-
-          meta = {
-            description = "Lura - simple cli roguelike game";
-            license = pkgs.lib.licenses.mit;
-          };
+        pkgs = import nixpkgs {
+          inherit system;
         };
-
+        go = pkgs.go_1_24;
+        
+        packageName = "lura";
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = packageName;
+          version = "1.2.0";
+          
+          src = ./.;
+          
+          vendorHash = "sha256-E5RjjrBus7iECoz+09mUCMWNGuJFT8aeI+qyAHR5xKs=";
+          
+          proxyVendor = true;
+          
+          env = {
+            CGO_ENABLED = "0";
+          };
+          
+          ldflags = ["-s" "-w" "-extldflags '-static'"];
+          
+          # preBuild = ''
+          #   go mod vendor
+          # '';
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
-            gopls       
-            #golangci-lint
+            gopls
+          #  gocode
+            delve
+            gotestsum
+            git
           ];
-
           shellHook = ''
+            export GOPATH=$HOME/go
+            export PATH=$GOPATH/bin:$PATH
+            export GO111MODULE=on
+            
+            export PROJECT_ROOT=$(pwd)
+            
+            echo "Go development environment ready!"
           '';
         };
       }
     );
 }
-
